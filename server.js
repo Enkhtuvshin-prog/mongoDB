fa9bd7ac8e91c382e81cd34b7321c68cecc5e50a
Enkhtvshn <116425332+Enkhtvshn@users.noreply.github.com>
2023-03-21 12:51:06 +0800

    multer

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
-const multer = require("multer");
+// const multer = require("multer");
+const upload = require("./uploads/upload");
 dotenv.config();
 
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
 const PORT = process.env.PORT;
 
 const connectDB = require("./config/mongoDb.js");
 const logger = require("./Logger");
 const userRoutes = require("./Routes/user");
 const categoryRoutes = require("./Routes/category");
+//instance of express
 const app = express();
 //middlewares
 app.use(express.json());
 app.use(logger);
+app.use("/uploads", express.static("uploads"));
 // const DATABASE_URI = process.env.DATABASE_URI;
 app.use("/users", userRoutes);
 app.use("/category", categoryRoutes);
@@ -23,8 +26,9 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/upload", upload.single("image"), (req, res) => {
-  // console.log("REQ", req);
-  res.status(200).json({ message: "SUCCESS" });
+  console.log("REQ", req.file);
+  res.status(200).json({ message: "SUCCESS", 
+  imgUrl: `${req.protocol}://${req.hostname}:${PORT}/${req.file.path}` });
 });
 connectDB();
 app.listen(PORT, () => {
